0b49f04df91cb164cb5b92f30c3f3a96eebe6d08
Garvit Gupta <garvitgupta2606@ce.du.ac.in>
2025-06-09 17:54:34 +0530

    new font

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import React from 'react'
 import UnderConstruction from './pages/UnderConstruction'
+import './app.css'
 
 const App = () => {
   return (
